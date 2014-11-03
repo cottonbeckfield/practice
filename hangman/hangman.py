422f9f1f793a3b567099d8471d9ae6b5e538dfb3
Courtney C <courtneycotton@berkeley.edu>
2014-11-03 13:09:14 -0800

    Initial commit.

---
+++ hangman/hangman.py
@@ -0,0 +1,15 @@
+import random
+
+# Create word list? / Variables
+# Allow Word list to be populated randomly. Pull in from file, perhaps?
+hangmanWords = ("story of the year")
+randomNumber = random.random.randint(0, len(hangmanWords) -1)
+# Draw board, wrong letters, guessed letters, etc.
+
+# Create board.
+
+# Create way to guess letters.
+
+# Game functionality.
+
+# Main Function
